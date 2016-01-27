e227bf9633581221f0f1a01d27c63a94dab9ade6
Najib <mnajib@gmail.com>
2016-01-27 09:51:20 +0800

    files copied

---
+++ README.md
@@ -0,0 +1,1 @@
+Please run ./setup.sh to setup after clone
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+#ln -s ./.vim ~/.vim
+#ln -s ./.vimrc ~/.vimrc
+#ln -s ./.tmux.conf ~/.tmux.conf
+#ln -s ./.gitconfig ~/.gitconfig
+
+ln -vsf $HOME/najib-config/.vimrc $HOME/.vimrc
+ln -vsf $HOME/najib-config/.tmux.conf $HOME/.tmux.conf
+ln -vsf $HOME/najib-config/.gitconfig $HOME/.gitconfig
